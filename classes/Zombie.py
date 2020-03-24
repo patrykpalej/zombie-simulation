bd506f8e8eed936a2521536b3cabe605e1e256d7
Patryk Palej <patrykpalej@gmail.com>
2020-03-24 20:41:30 +0100

    Implement basic gradient approach to zombies' moving strategy

--- classes/Zombie.py
+++ classes/Zombie.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Zombie:
     def __init__(self, zombies_params):
         self.x = zombies_params["x"]
@@ -26,6 +29,10 @@ class Zombie:
             y_h.append(human.y)
             smell.append(human.smell)
 
+        xh_vec = np.array(x_h)
+        yh_vec = np.array(y_h)
+        s_vec = np.array(smell)
+
         # 2. Put information about the zombie into variables // !!! TO CONSIDER
         x = self.x
         y = self.y
@@ -33,9 +40,18 @@ class Zombie:
         n = self.nose
 
         # 3. Calculate velocity coordinates
-        # ... some calculations ...
-        # v_x = v*gamma_hat_x
-        # v_y = v*gamma_hat_y
+        gamma_x = -(n+1) * sum(s_vec*(x-xh_vec) /
+                               ((x-xh_vec)**2+(y-yh_vec)**2)**((n+3)/2))
+
+        gamma_y = -(n + 1) * sum(s_vec * (y - yh_vec) /
+                                 ((x-xh_vec)**2+(y-yh_vec)**2)**((n+3)/2))
+
+        gamma_module = np.sqrt(gamma_x**2 + gamma_y**2)
+        gamma_hat_x = gamma_x / gamma_module
+        gamma_hat_y = gamma_y / gamma_module
+
+        v_x = v*gamma_hat_x
+        v_y = v*gamma_hat_y
 
         # 4. Implement displacement and changing direction in case the
         # water is nearby
@@ -44,7 +60,7 @@ class Zombie:
 
         # - - - - - - -
         # - - - - - - -
-        new_x = self.x + 1
-        new_y = self.y + 1
+        new_x = self.x + v_x
+        new_y = self.y + v_y
 
         return [new_x, new_y]
