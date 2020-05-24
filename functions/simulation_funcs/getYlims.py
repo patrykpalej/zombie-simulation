e8f2e45e80764c1b6fd9edeab8939259b7e5c517
Patryk Palej <patrykpalej@gmail.com>
2020-05-24 18:40:40 +0200

    Modify ylims to avoid max([])

--- functions/simulation_funcs/getYlims.py
+++ functions/simulation_funcs/getYlims.py
@@ -3,11 +3,11 @@ def get_ylims(humans, zombies):
     Returns ylims for showing characters statistics
     """
     # 1. Calculate max values for the y_lims
-    max_stamina = max([h.stamina for h in humans])
+    max_stamina = max([h.stamina for h in humans] + [0])
     max_battle_points = 1.1*max([h.battle_points for h in humans]
                                 + [z.battle_points for z in zombies])
-    max_truevelo = max([h.true_velo for h in humans])
-    max_smell = 1.1*max([h.smell for h in humans])
+    max_truevelo = max([h.true_velo for h in humans] + [0])
+    max_smell = 1.1*max([h.smell for h in humans] + [0])
     max_eyenose = 1.1*max([h.eye for h in humans] + [z.nose for z in zombies])
 
     # 2. Create lists of max ylims
